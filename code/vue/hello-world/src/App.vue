c7ecd13a35babb290a608ee9b2814a18f4feafa3
Philipp Schmutterer <trashmail837857@gmail.com>
2023-01-25 20:17:21 +0100

    created a hello world component in vue

---
+++ code/vue/hello-world/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <HelloWorld msg="Hello, World!"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
