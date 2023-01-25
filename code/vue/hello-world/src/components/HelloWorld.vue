354f13a2cdf24abc9737eb5efa76699160532892
Philipp Schmutterer <trashmail837857@gmail.com>
2023-01-25 20:19:17 +0100

    created a hello world component in vue

--- code/vue/hello-world/src/components/HelloWorld.vue
+++ code/vue/hello-world/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>{{ msg }}</h1>
+    <h1>Hello, World!</h1>
 </template>
 
 <script lang="ts">
@@ -7,13 +7,9 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   h1{color: green;}
 </style>
